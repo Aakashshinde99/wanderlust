d5263d0acd572001b0b5f39f1dda90b1c20a5e2d
Aakash Shinde <70742739+Aakashshinde99@users.noreply.github.com>
2024-10-17 12:05:08 +0530

    Update api-error.js

--- backend/utils/api-error.js
+++ backend/utils/api-error.js
@@ -1,28 +1,11 @@
 import { Error as MongooseError } from 'mongoose';
 import { RESPONSE_MESSAGES } from './constants.js';
 
-interface ApiErrorParams {
-  status: number;
-  message?: string;
-  errors?: any[];
-  stack?: string;
-}
-
 class ApiError extends MongooseError {
-  public status: number;
-  public data: null;
-  public success: boolean;
-  public errors: any[];
-  constructor({
-    status,
-    message = RESPONSE_MESSAGES.COMMON.SOMETHING_WRONG,
-    errors = [],
-    stack = '',
-  }: ApiErrorParams) {
-    super(message);
+  constructor({ status, message, errors = [], stack = '' }) {
+    super(message || RESPONSE_MESSAGES.COMMON.SOMETHING_WRONG);
     this.status = status;
     this.data = null;
-    this.message = message;
     this.success = false;
     this.errors = errors;
 
